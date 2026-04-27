ba18a8ecb929362d43af71dca6444c1c47bb4f40
mgilani-cyber <mgilani@etherealpr.com>
2026-04-27 17:07:53 -0400

    Create SEOHead.tsx

---
+++ src/components/SEOHead.tsx
@@ -0,0 +1,11 @@
+interface SEOHeadProps {
+  title?: string;
+  description?: string;
+  keywords?: string;
+}
+
+const SEOHead = ({ title, description, keywords }: SEOHeadProps) => {
+  return null;
+};
+
+export default SEOHead;
